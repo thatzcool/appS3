3c6d1415807358ba34893f7a82caf41a856f9695
yumi <mycaptain0771@gmail.com>
2022-12-08 13:11:15 +0900

    s3commit

---
+++ src/main/java/net/ict/apps3/dto/SampleDTO.java
@@ -0,0 +1,15 @@
+package net.ict.apps3.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SampleDTO {
+    private MultipartFile[] files;
+}
---
+++ src/test/java/net/ict/apps3/AppS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ict.apps3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppS3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
